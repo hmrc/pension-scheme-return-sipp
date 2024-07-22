5affd82ceec5811eb7bf404f9d5abbd33cdb28fe
feyyazrc <95756811+feyyazrc@users.noreply.github.com>
2024-07-22 12:25:42 +0100

    PSR-1286 - resolve conflicts

--- app/uk/gov/hmrc/pensionschemereturnsipp/controllers/EmailResponseController.scala
+++ app/uk/gov/hmrc/pensionschemereturnsipp/controllers/EmailResponseController.scala
@@ -16,22 +16,6 @@
 
 package uk.gov.hmrc.pensionschemereturnsipp.controllers
 
-/*
- * Copyright 2024 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import com.google.inject.Inject
 import play.api.Logger
 import play.api.libs.json.JsValue
@@ -40,7 +24,8 @@ import uk.gov.hmrc.auth.core.{AuthConnector, AuthorisedFunctions}
 import uk.gov.hmrc.crypto.{ApplicationCrypto, Crypted}
 import uk.gov.hmrc.pensionschemereturnsipp.audit.PsrEmailAuditEvent
 import uk.gov.hmrc.pensionschemereturnsipp.config.Constants.emailRegex
-import uk.gov.hmrc.pensionschemereturnsipp.models.{EmailEvents, Opened}
+import uk.gov.hmrc.pensionschemereturnsipp.models.EmailEvents
+import uk.gov.hmrc.pensionschemereturnsipp.models.Event.Opened
 import uk.gov.hmrc.pensionschemereturnsipp.services.AuditService
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
