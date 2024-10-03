04121c8d550c513bfe387cdec645f16b7cfd8fa1
feyyazrc <95756811+feyyazrc@users.noreply.github.com>
2024-10-03 11:06:09 +0100

    PSR-1476: Delete mockito dependency as it's already provided by bootstrap-test-play

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "org.typelevel"                 %% "cats-core"                  % "2.12.0",
-    "com.networknt"                 % "json-schema-validator"       % "1.5.1" exclude("org.slf4j", "slf4j-api"),
+    "com.networknt"                 %  "json-schema-validator"      % "1.5.1" exclude("org.slf4j", "slf4j-api"),
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.17.2",
     "com.beachape"                  %% "enumeratum-play-json"       % "1.8.1",
     "io.scalaland"                  %% "chimney"                    % "1.3.0"
@@ -20,7 +20,6 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion,
     "org.scalatestplus"             %% "scalacheck-1-17"            % "3.2.18.0",
     "org.scalacheck"                %% "scalacheck"                 % "1.18.0",
-    "org.scalatestplus"             %% "mockito-3-4"                % "3.2.10.0",
     "com.softwaremill.diffx"        %% "diffx-scalatest-should"     % "0.9.0",
     "com.vladsch.flexmark"          % "flexmark-all"                % "0.64.8",
   ).map(_ % Test)
