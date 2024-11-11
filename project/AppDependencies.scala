38170b885e46c49fbaa3b466745b3af54aaaecc5
Chris Gamble <chris.gamble@digital.hmrc.gov.uk>
2024-11-11 10:17:24 +0000

    Remove mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,6 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "org.typelevel"                 %% "cats-core"                  % "2.12.0",
     "com.networknt"                 %  "json-schema-validator"      % "1.5.1" exclude("org.slf4j", "slf4j-api"),
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.17.2",
@@ -17,7 +16,6 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-test-play-30"     % bootstrapVersion,
-    "uk.gov.hmrc.mongo"             %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion,
     "org.scalatestplus"             %% "scalacheck-1-17"            % "3.2.18.0",
     "org.scalacheck"                %% "scalacheck"                 % "1.18.0",
     "org.scalatestplus"             %% "mockito-4-11"               % "3.2.18.0",
