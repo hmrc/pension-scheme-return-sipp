702de15a462666139caf709f79fb9848f7bbcacf
narendrapaduchuri <narendra.paduchuri@digital.hmrc.gov.uk>
2025-06-13 15:31:41 +0100

    PSR-1982 bootstrapVer upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.12.0"
+  private val bootstrapVersion = "9.13.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
